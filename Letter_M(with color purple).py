58245f65ac944dc212b887c5284d74b4a665bc05
mar2414-shal <marshal.sahoo.2412@gmail.com>
2025-12-03 18:45:37 +0800

    Letter_M(with color purple).py
    This is also my initial letter with a color

---
+++ Letter_M(with color purple).py
@@ -0,0 +1,15 @@
+import turtle
+t = turtle.Turtle()
+
+t.color("purple")
+t.pensize(5)
+t.speed(3)
+
+t.left(90)
+t.forward(100)
+t.right(135)
+t.forward(100)
+t.left(95)
+t.forward(100)
+t.right(140)
+t.forward(105)
